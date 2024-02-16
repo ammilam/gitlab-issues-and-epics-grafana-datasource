1eea87edf9ad6f21fffa2969f66eccc949fe849f
Andrew Milam <andrewmichaelmilam@gmail.com>
2024-02-15 23:29:16 -0700

    fix: app

--- caching-server/app.js
+++ caching-server/app.js
@@ -6,29 +6,23 @@ const fs = require('fs');
 app.use(express.json());
 const { startCron, writeFile } = require('./gitlab');
 
-// const cors = require('cors');
-
-// const corsOptions = {
-//   origin: origin, // Allow requests from Grafana service DNS
-//   methods: "GET, HEAD, PUT, PATCH, POST, DELETE",
-//   'Access-Control-Allow-Origin': origin, // Allow CORS for Grafana service DNS
-// };
-
-// Use CORS middleware
-// app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, HEAD, PUT, PATCH, POST, DELETE');
+  next();
+});
 
 app.get('/gitlab', async (req, res) => {
   try {
-
     let query = req.query;
 
     if (!query.group) {
-      res.status(400).json({ error: "web request missing 'group' query string" })
+      res.status(400).json({ error: "web request missing 'group' query string" });
     }
 
     let group = query.group;
 
-    console.log(`Getting data for group ${group}`)
+    console.log(`Getting data for group ${group}`);
 
     let groupData = `./data/${group}.json`;
 
@@ -41,74 +35,72 @@ app.get('/gitlab', async (req, res) => {
     res.status(200).json({ data: base64 });
 
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: error.message })
+    console.log(error);
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
 app.get('/issues', async (req, res) => {
-
   try {
     let query = req.query;
 
     if (!query.group) {
-      res.status(400).json({ error: "web request missing 'group' query string" })
+      res.status(400).json({ error: "web request missing 'group' query string" });
     }
 
     let group = query.group;
 
-    console.log(`Getting data for group ${group}`)
+    console.log(`Getting data for group ${group}`);
 
     let groupData = `./data/${group}.json`;
 
     let data = fs.readFileSync(groupData);
 
     // buffer the data and base64 encode
-    let buf = Buffer.from(data).toString()
+    let buf = Buffer.from(data).toString();
 
-    res.status(200).json({ issues: JSON.parse(buf)['issues'] })
+    res.status(200).json({ issues: JSON.parse(buf)['issues'] });
 
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: error.message })
+    console.log(error);
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
 app.get('/epics', async (req, res) => {
-
   try {
     let query = req.query;
 
     if (!query.group) {
-      res.status(400).json({ error: "web request missing 'group' query string" })
+      res.status(400).json({ error: "web request missing 'group' query string" });
     }
 
     let group = query.group;
 
-    console.log(`Getting data for group ${group}`)
+    console.log(`Getting data for group ${group}`);
 
     let groupData = `./data/${group}.json`;
 
     let data = fs.readFileSync(groupData);
 
     // buffer the data and base64 encode
-    let buf = Buffer.from(data).toString()
+    let buf = Buffer.from(data).toString();
 
-    res.status(200).json({ epics: JSON.parse(buf)['epics'] })
+    res.status(200).json({ epics: JSON.parse(buf)['epics'] });
 
   } catch (error) {
-    console.log(error)
-    res.status(500).json({ error: error.message })
+    console.log(error);
+    res.status(500).json({ error: error.message });
   }
-})
+});
 
-app.get('/health', async (req, res) => {
-  console.log('health check')
-  res.status(200).json({ message: "ok" })
-})
+app.get('/health', async (_, res) => {
+  console.log('health check');
+  res.status(200).json({ status: "ok" });
+});
 
 app.listen(PORT, () => {
-  writeFile()
-  startCron()
+  writeFile();
+  startCron();
   console.log(`Proxy server running on http://localhost:${PORT}`);
 });
